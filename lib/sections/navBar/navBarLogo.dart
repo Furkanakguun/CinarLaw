9eb6c6d213e6155fc0acf74728b63fd259f49c4e
FURKAN AKGUN <fakgun@yukselproje.com.tr>
2022-02-28 01:10:25 +0300

    new design home and navbar

--- lib/sections/navBar/navBarLogo.dart
+++ lib/sections/navBar/navBarLogo.dart
@@ -9,22 +9,12 @@ class NavBarLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _themeProvider = Provider.of<ThemeProvider>(context);
-    return Padding(
-      padding: EdgeInsets.fromLTRB(
-          MediaQuery.of(context).size.width < 1100 ? 0.0 : 20.0, 20.0, 0, 20),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            "Çınar&Çınar",
-            style: GoogleFonts.brygada1918(
-              color: _themeProvider.lightTheme ? Colors.black : Colors.white,
-              fontSize:22
-                //       color: kPrimaryColor, fontSize: height * 0.025),
-                // ),
-                ),
-          ),
-        ],
+    return Center(
+      child: Image.asset(
+        'assets/cinar_Logo.png',
+        //fit: BoxFit.fill,
+        height: 80,
+        width: 160,
       ),
     );
   }
